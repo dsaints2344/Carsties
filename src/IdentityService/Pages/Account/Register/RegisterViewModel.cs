ae377cbe10fa1e54126a42de13532448b8b6ea19
David De Los Santos <ddelossantos@neural.com.do>
2024-03-07 14:58:02 -0400

    WIP: Register page

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Duende.IdentityServer.Services;
+
+namespace IdentityService.Pages.Account.Register
+{
+    public class RegisterViewModel
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Username { get; set; }
+        public string? FullName { get; set; }
+        public string?  ReturnUrl { get; set; }
+        public string? Button { get; set; }
+    }
+}
