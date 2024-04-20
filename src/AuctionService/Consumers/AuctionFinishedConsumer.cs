4538dfd79ac21138686727e40415916a16e5f7d0
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2024-04-20 19:03:47 -0400

    Adding consumers for auction service

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,39 @@
+﻿using AuctionService.Data;
+using AuctionService.Data.Enums;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        private AuctionDbContext _dbContext;
+        
+        public AuctionFinishedConsumer(AuctionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+
+            Console.WriteLine("--> Consuming Auction Finished");
+
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            if (auction != null)
+            {
+                if (context.Message.ItemSold)
+                {
+                    auction.Winner = context.Message.Winner;
+                    auction.SoldAmount = context.Message.Amount;
+                }
+
+                auction.Status = auction.SoldAmount > auction.ReservePrice ?
+                    Status.Finished : Status.ReserveNotMet;
+            }
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
