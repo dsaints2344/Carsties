d07a8e3273d19c28008b3051270ab175bb7353e8
David De Los Santos <david.mateo@helixsolution.com>
2024-10-30 12:12:26 -0400

    Update Search.tsx

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -1,14 +1,42 @@
+"use client";
+
 import { FaSearch } from "react-icons/fa";
+import { useParamsStore } from "../hooks/useParamsStore";
+import { useState } from "react";
 
 const Search = () => {
+    const setParams = useParamsStore(state => state.setParams);
+    const [value, setValue] = useState("");
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const onChange = (event: any) => {
+        setValue(event.target.value);
+    }
+
+    const search = () => {
+        setParams({searchTerm: value});
+    }
+
     return (
         <div className="flex w-[50%]  items-center border-2 rounded-full py-2 shadow-sm">
             <input
+                value={value}
                 type="text"
                 placeholder="Search for cars by make, model or color" 
+                className="flex-grow pl-5 bg-transparent focus:outline-none 
+                    border-transparent focus:border-transparent focus:ring-0
+                    text-sm text-gray-600"
+                onChange={onChange}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        search();
+                    }
+                }}
             />
-            <button>
-                <FaSearch/>
+            <button onClick={search}>
+                <FaSearch 
+                    size={34} 
+                    className="bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2"/>
             </button>
         </div>
     )
