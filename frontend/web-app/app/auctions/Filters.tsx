3cc6af4f0e7e6d77728bf1843116d12d72bd33c5
David De Los Santos <david.mateo@helixsolution.com>
2025-08-01 21:40:12 -0400

    [Section 8.1010] Add order by filter to auction listings
    Introduces an 'Order By' filter in the auction filters UI, allowing users to sort listings by alphabetical order, end date, or recently added. Updates the params store and listings component to support and propagate the new 'orderBy' parameter.

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,13 +1,38 @@
 import { Button, ButtonGroup } from "flowbite-react";
 import { useParamsStore } from "../hooks/useParamsStore";
+import { AiOutlineClockCircle, AiOutlineSortAscending } from "react-icons/ai";
+import { BsFillStopCircleFill } from "react-icons/bs";
 
 const pageSizeButtons = [ 4, 8, 12 ];
+const orderButtons = [
+    {label: "Alphabetical", icon: AiOutlineSortAscending, value: "make"},
+    {label: "End date", icon: AiOutlineClockCircle, value: "endingSoon"},
+    {label: "Recently Added", icon: BsFillStopCircleFill, value: "new"}
+];
+
 
 const Filters = () => {
     const pageSize = useParamsStore(state => state.pageSize);
     const setParams = useParamsStore(state => state.setParams);
+    const orderBy = useParamsStore(state => state.orderBy);
+
     return (
         <div className="flex justify-between items-center mb-4">
+            <div>
+                <span className="uppercase text-sm text-gray-500 mr-2">Order By</span>
+                <ButtonGroup>
+                    {orderButtons.map(({label, icon: Icon, value}) => (
+                        <Button key={value} onClick={() => setParams({orderBy: value})}
+                            color={`${orderBy === value ? 'red' : 'gray'}`}
+                            className="focus:ring-0"
+                        >
+                            <Icon className="mr-3 h-4 w-4"/>
+                            {label}
+                        </Button>
+                    ))}
+                </ButtonGroup>
+            </div>
+
             <div>
                 <span className="uppercase text-sm text-gray-500 mr-2">Page Size</span>
                 <ButtonGroup>
