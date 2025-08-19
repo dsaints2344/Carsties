d78dd5e898eeaccec8bf954ca5b2cb3035d44fbf
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2025-08-18 20:07:26 -0400

    Add AuctionForm component and update create auction page
    Introduced a new AuctionForm component for auction creation. Updated the create auction page to use AuctionForm and improved its layout and heading. Added react-datepicker and react-hook-form dependencies for future form enhancements.

---
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -0,0 +1,9 @@
+const AuctionForm = () => {
+    return (
+        <div>
+            <h1>Create Auction</h1>
+        </div>
+    );
+}
+
+export default AuctionForm;
--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,6 +1,12 @@
+import Heading from "@/app/components/Heading";
+import AuctionForm from "../AuctionForm";
+
 const Create = () => {
     return (
-        <div>Create Auction Form !!</div>
+        <div className="mx-[10%] max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+            <Heading title="Sell your car!" subtitle="Please enter the details of your car"/>
+            <AuctionForm />
+        </div>
     );
 }
 
