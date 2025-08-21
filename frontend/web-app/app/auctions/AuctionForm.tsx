68ec63eec95dbbd2938f3085a2938300632fd846
David De Los Santos <david.mateo@helixsolution.com>
2025-08-20 21:42:03 -0400

    Add auction creation form with validation
    Implemented a form for creating auctions using react-hook-form and Flowbite UI components. Added validation for 'make' and 'model' fields, error handling, and submit/cancel actions.

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,8 +1,57 @@
+"use client";
+
+import { Button, HelperText, Spinner, TextInput } from "flowbite-react";
+import { useRouter } from "next/navigation";
+import { FieldValues, useForm } from "react-hook-form";
+
 const AuctionForm = () => {
+    const router = useRouter();
+    const { register, handleSubmit, setFocus, 
+        formState: { isSubmitting, isValid, isDirty, errors }
+    } = useForm();
+
+    const onSubmit = (data: FieldValues) => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h1>Create Auction</h1>
-        </div>
+        <form className="flex flex-col mt3" onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3 block">
+                <TextInput
+                    {...register("make", {
+                        required: "Make is required"
+                    })} 
+                    placeholder="Make"
+                    color={errors?.make && "failure"}
+                />
+                <HelperText color="failure">{errors?.make?.message as string}</HelperText>
+            </div>
+            <div className="mb-3 block">
+                <TextInput
+                    {...register("model", {
+                        required: "Model is required"
+                    })} 
+                    placeholder="Model"
+                    color={errors?.model && "failure"}
+                />
+                <HelperText color="failure">{errors?.model?.message as string}</HelperText>
+            </div>
+
+            <div className="flex justify-between">
+                <Button  
+                    color="light" 
+                    onClick={() => router.push("/")}>Cancel</Button>
+                <Button
+                    color="green"
+                    disabled={!isDirty || !isValid}
+                    type="submit"
+                >
+                    {isSubmitting && <Spinner size="sm" />}
+                    Submit
+                </Button>
+            </div>
+
+        </form>
     );
 }
 
