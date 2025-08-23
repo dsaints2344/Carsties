40397221c003c96bf5976c93e748b219fceded9b
David De Los Santos <david.mateo@helixsolution.com>
2025-08-22 21:23:59 -0400

    Refactor auction actions to use fetchWrapper utility
    Introduced a fetchWrapper utility for standardized API requests with authentication and error handling. Updated auctionActions to use fetchWrapper for GET and PUT requests, improving code reuse and maintainability.

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,17 +1,10 @@
 "use server";
 
-import { auth } from "@/auth";
+import { fetchWrapper } from "@/lib/fetchWrapper";
 import { Auction, PagedResult } from "@/types";
 
 const getData = async (query: string): Promise<PagedResult<Auction>> => {
-    const res = await fetch(`http://localhost:6001/search${query}`);
-
-    if (!res.ok){
-        throw new Error('Failed to fetch data');
-    }
-    else {
-        return res.json();
-    }
+    return fetchWrapper.get(`search${query}`);
 }
 
 const updateAuctionTest = async (): Promise<{status: number, message: string}> => {
@@ -19,22 +12,8 @@ const updateAuctionTest = async (): Promise<{status: number, message: string}> =
         mileage: Math.floor(Math.random() * 10000) + 1
     };
 
-    const session = await auth();
+    return fetchWrapper.put(`auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c`, data);
 
-    const res = await fetch(`http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${session?.accessToken}`
-        },
-        body: JSON.stringify(data)
-    });
-
-    if (!res.ok) {
-        return {status: res.status, message: res.statusText}
-    }
-
-    return {status: res.status, message: res.statusText};  
 }
 
 export { getData, updateAuctionTest };
